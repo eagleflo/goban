54ce9160d595abfef95e0bea65c29888fc7dc9bc
Aku Kotkavuo <aku@hibana.net>
2008-06-01 02:14:03 +0300

    Initial commit.

---
+++ go.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Go library made with pure Python.
+
+This library offers a variety of Go related classes and methods.
+
+There is a companion module called 'goban' which serves as a front-end
+for this library, forming a fully working go board together.
+
+"""
+
+__author__ = "Aku Kotkavuo <aku@hibana.net>"
+__version__ = "0.1"
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+class Stone(object):
+    def __init__(self, board, point, color):
+        """Create and initialize a stone.
+        
+        Arguments:
+        board -- the board which the stone resides on
+        point -- location of the stone as a tuple, e.g. (3, 3)
+                 represents the upper left hoshi
+        color -- color of the stone
+        
+        """
+        self.board = board
+        self.point = point
+        self.color = color
+        self.group = self.find_group()
+
+    def remove(self):
+        """Remove the stone from board."""
+        self.group.stones.remove(self)
+        del self
+
+    @property
+    def neighbors(self):
+        """Return a list of neighboring points."""
+        neighboring = [(self.point[0]-1, self.point[1]),
+                       (self.point[0]+1, self.point[1]),
+                       (self.point[0], self.point[1]-1),
+                       (self.point[0], self.point[1]+1)]
+        for point in neighboring:
+            if not 0 < point[0] < 20 or not 0 < point[1] < 20:
+                neighboring.remove(point)
+        return neighboring
+
+    @property
+    def liberties(self):
+        """Find and return the liberties of the stone."""
+        liberties = self.neighbors
+        stones = self.board.search(points=self.neighbors)
+        for stone in stones:
+            liberties.remove(stone.point)
+        return liberties
+
+    def find_group(self):
+        """Find or create a group for the stone."""
+        groups = []
+        stones = self.board.search(points=self.neighbors)
+        for stone in stones:
+            if stone.color == self.color and stone.group not in groups:
+                groups.append(stone.group)
+        if not groups:
+            group = Group(self.board, self)
+            return group
+        else:
+            if len(groups) > 1:
+                for group in groups[1:]:
+                    groups[0].merge(group)
+            groups[0].stones.append(self)
+            return groups[0]
+
+    def __str__(self):
+        """Return the location of the stone, e.g. 'D17'."""
+        return 'ABCDEFGHJKLMNOPQRST'[self.point[0]-1] + str(20-(self.point[1]))
+
+
+class Group(object):
+    def __init__(self, board, stone):
+        """Create and initialize a new group.
+        
+        Arguments:
+        board -- the board which this group resides in
+        stone -- the initial stone in the group
+        
+        """
+        self.board = board
+        self.board.groups.append(self)
+        self.stones = [stone]
+        self.liberties = None
+
+    def merge(self, group):
+        """Merge two groups.
+        
+        This method merges the argument group with this one by adding
+        all its stones into this one. After that it removes the group
+        from the board.
+        
+        Arguments:
+        group -- the group to be merged with this one
+        
+        """
+        for stone in group.stones:
+            stone.group = self
+            self.stones.append(stone)
+        self.board.groups.remove(group)
+        del group
+
+    def remove(self):
+        """Remove the entire group."""
+        while self.stones:
+            self.stones[0].remove()
+        self.board.groups.remove(self)
+        del self
+
+    def update_liberties(self):
+        """Update the group's liberties.
+        
+        As this method will remove the entire group if no liberties can
+        be found, it should only be called once per turn.
+        
+        """
+        liberties = []
+        for stone in self.stones:
+            for liberty in stone.liberties:
+                liberties.append(liberty)
+        self.liberties = set(liberties)
+        if len(self.liberties) == 0:
+            self.remove()
+
+    def __str__(self):
+        """Return a list of the group's stones as a string."""
+        return str([str(stone) for stone in self.stones])
+
+class Board(object):
+    def __init__(self):
+        """Create and initialize an empty board."""
+        self.groups = []
+        self.next = BLACK
+
+    def search(self, point=None, points=[]):
+        """Search the board for a stone.
+        
+        The board is searched in a linear fashion, looking for either a
+        stone in a single point (which the method will immediately
+        return if found) or all stones within a group of points.
+        
+        Arguments:
+        point -- a single point (tuple) to look for
+        points -- a list of points to be searched
+        
+        """
+        stones = []
+        for group in self.groups:
+            for stone in group.stones:
+                if stone.point == point and not points:
+                    return stone
+                if stone.point in points:
+                    stones.append(stone)
+        return stones
+
+    def turn(self):
+        """Keep track of the turn by flipping between BLACK and WHITE."""
+        if self.next == BLACK:
+            self.next = WHITE
+            return BLACK
+        else:
+            self.next = BLACK
+            return WHITE
---
+++ goban.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Goban made with Python, pygame and go.py.
+
+This is a front-end for my go library 'go.py', handling drawing and
+pygame-related activities. Together they form a fully working goban.
+
+"""
+
+__author__ = "Aku Kotkavuo <aku@hibana.net>"
+__version__ = "0.1"
+
+import pygame
+import go
+from sys import exit
+
+BACKGROUND = 'images/ramin.jpg'
+BOARD_SIZE = (820, 820)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+class Stone(go.Stone):
+    def __init__(self, board, point, color):
+        """Create, initialize and draw a stone."""
+        super(Stone, self).__init__(board, point, color)
+        self.coords = (5+self.point[0]*40, 5+self.point[1]*40)
+        self.draw()
+
+    def draw(self):
+        """Draw the stone as a circle."""
+        pygame.draw.circle(screen, self.color, self.coords, 20, 0)
+        pygame.display.update()
+
+    def remove(self):
+        """Remove the stone from board."""
+        blit_coords = (self.coords[0]-20, self.coords[1]-20)
+        area_rect = pygame.Rect(blit_coords, (40, 40))
+        screen.blit(background, blit_coords, area_rect)
+        pygame.display.update()
+        super(Stone, self).remove()
+
+
+class Board(go.Board):
+    def __init__(self):
+        """Create, initialize and draw an empty board."""
+        super(Board, self).__init__()
+        self.outline = pygame.Rect(45, 45, 720, 720)
+        self.draw()
+
+    def draw(self):
+        """Draw the board to the background and blit it to the screen.
+
+        The board is drawn by first drawing the outline, then the 19x19
+        grid and finally by adding hoshi to the board. All these
+        operations are done with pygame's draw functions.
+
+        This method should only be called once, when initializing the
+        board.
+
+        """
+        pygame.draw.rect(background, BLACK, self.outline, 3)
+        # Outline is inflated here for future use as a collidebox for the mouse
+        self.outline.inflate_ip(20, 20)
+        for i in range(18):
+            for j in range(18):
+                rect = pygame.Rect(45+(40*i), 45+(40*j), 40, 40)
+                pygame.draw.rect(background, BLACK, rect, 1)
+        for i in range(3):
+            for j in range(3):
+                coords = (165+(240*i), 165+(240*j))
+                pygame.draw.circle(background, BLACK, coords, 5, 0)
+        screen.blit(background, (0,0))
+        pygame.display.update()
+
+
+def main():    
+    while True:
+        pygame.time.wait(250)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1 and board.outline.collidepoint(event.pos):
+                    x = int(round(((event.pos[0]-5) / 40.0), 0))
+                    y = int(round(((event.pos[1]-5) / 40.0), 0))
+                    stone = board.search(point=(x, y))
+                    if stone:
+                        stone.remove()
+                    else:
+                        stone = Stone(board, (x, y), board.turn())
+        for group in board.groups:
+            group.update_liberties()
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Goban')
+    screen = pygame.display.set_mode(BOARD_SIZE, 0, 32)
+    background = pygame.image.load(BACKGROUND).convert()
+    board = Board()
+    main()
